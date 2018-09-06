2a68c90aaf56b5a92cff3800d90105794818724d
YSG12 <6179466852@qq.com>
2018-09-06 23:24:52 +0800

    添加图书爬虫

--- book/book/settings.py
+++ book/book/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for yangguang project
+# Scrapy settings for book project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -9,15 +9,14 @@
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'yangguang'
+BOT_NAME = 'book'
 
-SPIDER_MODULES = ['yangguang.spiders']
-NEWSPIDER_MODULE = 'yangguang.spiders'
+SPIDER_MODULES = ['book.spiders']
+NEWSPIDER_MODULE = 'book.spiders'
 
-# LOG_LEVEL = "WARNING"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+#USER_AGENT = 'book (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -28,18 +27,18 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3  # 下载延迟
+#DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False # 是否开启cookies
+#COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
 
-# Override the default request headers: 默认请求头
+# Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
@@ -48,13 +47,13 @@ ROBOTSTXT_OBEY = True
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'yangguang.middlewares.YangguangSpiderMiddleware': 543,
+#    'book.middlewares.BookSpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'yangguang.middlewares.YangguangDownloaderMiddleware': 543,
+#    'book.middlewares.BookDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -65,11 +64,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   'yangguang.pipelines.YangguangPipeline': 300,
-}
+#ITEM_PIPELINES = {
+#    'book.pipelines.BookPipeline': 300,
+#}
 
-# Enable and configure the AutoThrottle extension (disabled by default) 自动限速
+# Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
 # The initial download delay
@@ -82,7 +81,7 @@ ITEM_PIPELINES = {
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
-# Enable and configure HTTP caching (disabled by default) 缓存
+# Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
 #HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
--- yangguang/yangguang/pipelines.py
+++ yangguang/yangguang/pipelines.py
@@ -5,13 +5,21 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import re
+from pymongo import MongoClient
 
 class YangguangPipeline(object):
+
+    def open_spider(self, spider):
+        client = MongoClient(host='132.232.19.246', port=27017)
+        self.collection = client["tencent"]["test"]
+
     def process_item(self, item, spider):
         item["content"] = self.process_content(item["content"])
         # print(item)
+        self.collection.insert(dict(item))
         return item
 
     def process_content(self, content):
         content = [re.sub(r"\xa0|\s", "",i) for i in content] # 将\xa0字符串转换为空格
         content = [i for i in content if len(i) > 0] # 去除content中的空字符串
+        return content
